b27bf5db0443a20c8014e73305e7bb99241f121c
vijayta <vijayta.panchal@gmail.com>
2013-11-13 16:41:40 +0530

    minor fix

--- exercise01/exercise01.js
+++ exercise01/exercise01.js
@@ -1,7 +1,10 @@
 /*jslint devel: true */
-var compareAge = { check: function () {
-    var john = {name : "John", age : 40 }, mary = {name : "Mary", age : 10 };
-    if (john.age > mary.age) { alert("John is older than Mary"); }
-     else if (john.age < mary.age) { alert("Mary is older than John"); }
-     else { alert("Mary and John Are on same age"); }      
-}}
+var compareAge = { 
+  check: function () {
+    var john = {name : "John", age : 40 }, mary = {name : "Mary", age : 40 };
+    if(john.age < 0 || mary.age < 0) { alert("Age cant be nagetive"); }
+      else if (john.age > mary.age) { alert("John is older than Mary"); } 
+        else if (john.age < mary.age) { alert("Mary is older than John"); } 
+          else if(john.age == mary.age) { alert("Mary and John Are on same age"); } 
+  }
+}
