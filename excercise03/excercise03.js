9dcec3c94deaf2f45fbf4aecb25d2884c620f23a
vijayta <vijayta.panchal@gmail.com>
2014-02-04 11:26:07 +0530

    modifications

---
+++ excercise03/excercise03.js
@@ -0,0 +1,38 @@
+
+function checkLimit(j){
+  var total = 0;
+  list = document.signup.elements['list']; // Global Variable list
+  var max = list.length;
+  var txt ="";
+  for (var x = 0; x < max; x++) {
+  if (list[x].checked) {
+    total += 1;
+    if(total > 3)
+    { 
+      list[j].checked=false;
+      total--;
+      alert("you cant select more then 3 item at once you have already selected " + total + " : " + txt);
+      return false;
+
+    }
+     else {
+      txt += list[x].value + ' '; 
+    } 
+  }
+}
+  for(var z=0; z < max; z++){
+      if("list[" + z + "].checked"){
+      document.signup.uncheckall.checked=false; 
+    }
+  }
+}
+function unCheckAll(checkItem){
+  for (i = 0; i < checkItem.length; i++){
+    checkItem[i].checked = false ;
+  }
+}
+
+
+window.onload = function() {
+  document.getElementById('uncheckall').onclick = function() { unCheckAll(list); };
+}
