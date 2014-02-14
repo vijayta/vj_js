a0962ca5bbcc8c339d6a31c247a33e0f047e18c1
vijayta <vijayta.panchal@gmail.com>
2014-02-14 12:02:46 +0530

    minor fix

--- excercise03/excercise03.js
+++ excercise03/excercise03.js
@@ -1,19 +1,24 @@
 function onCheck(id){
   this.id=id;
-  var i, len, inputs;
-  var count = 0;
-  var array = [];
-  var inputs = document.weekdays.getElementsByTagName("input");
+  inputs = document.weekdays.getElementsByTagName("input");
+  
+}
+onCheck.prototype.checkLimit = function(val){  
+  var val = val;
   document.getElementById("none").checked = false;
+  var i, len;
+  var array = [];
+  var count = 0; 
   for (i = 0, len = inputs.length; i < len; i++) {
     if (inputs[i].type == "checkbox" && inputs[i].checked){
-      count = count+1;
-      if(count < 4){
+      count = count + 1;
+      if(count <= val){
         array[i] = inputs[i].id;
       }
       else{
         document.getElementById(inputs[i].id).checked=false;
-        alert("Only 3 days can be selected. You have already selected : "+array);
+        var text = array.join(" ");
+        alert("Only 3 days can be selected. You have already selected : " + text);
         return false;
       }
     }
@@ -34,11 +39,12 @@ window.onload=function(){
   none.addEventListener('click', function() {
     onClickNone()
   });
-
+  
   var inputs = document.weekdays.getElementsByTagName("input");
   for(var i = 0; i < inputs.length; i++){
     inputs[i].addEventListener('click', function() {
-      onCheck(this.id) 
+      var check_limit = new onCheck(this.id);
+      check_limit.checkLimit(3)
     });
   }
 }
