4db8fdf4d5a755aa43a33a2aa97e4432ff282efa
vijayta <vijayta.panchal@gmail.com>
2014-02-15 23:08:19 +0530

    js update

--- excercise08/excercise08.js
+++ excercise08/excercise08.js
@@ -1,21 +1,21 @@
-function set_url(){
-  this.webAdd = validateUrl(prompt("Enter URL: "));
+function SetUrl(){
+  this.webAdd = ValidateUrl(prompt("Enter URL: "));
   return this.webAdd;
 }
-function validateUrl(check_url){
+function ValidateUrl(check_url){
   this.check_url = check_url;
   if(check_url == "" || check_url == null){
     alert("The URL you enter is Empty");
-    return set_url();
+    return SetUrl();
   }
   return this.check_url;
 }
 
-set_url.prototype.dispURL = function(){
+SetUrl.prototype.DispURL = function(){
   window.open("http://" + this.webAdd, "", "height=450 , width=400, scrollbars=no ");
 }
 
 window.onload = function(){ 
-  var u = new set_url();
-  u.dispURL(); 
+  var u = new SetUrl();
+  u.DispURL(); 
 }
