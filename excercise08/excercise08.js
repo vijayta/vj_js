59350f57536812f1ebe4c4b4474595ef3987342a
vijayta <vijayta.panchal@gmail.com>
2014-02-20 14:30:16 +0530

    minor fixes

--- excercise08/excercise08.js
+++ excercise08/excercise08.js
@@ -1,21 +1,23 @@
-function SetUrl(){
-  this.webAdd = ValidateUrl(prompt("Enter URL: "));
+function getUrl(){
+  this.webAdd = serUrl(prompt("Enter URL: "));
   return this.webAdd;
 }
-function ValidateUrl(check_url){
-  this.check_url = check_url;
-  if(check_url == "" || check_url == null){
-    alert("The URL you enter is Empty");
-    return SetUrl();
+function serUrl(url){
+  this.url =  url;
+  if(url != "" || url != null){
+    return this.url;
   }
-  return this.check_url;
+  else{
+    alert("The URL you enter is Empty");
+    return getUrl();
+  }
 }
 
-SetUrl.prototype.DispURL = function(){
+getUrl.prototype.dispURL = function(){
   window.open("http://" + this.webAdd, "", "height=450 , width=400, scrollbars=no ");
 }
 
 window.onload = function(){ 
-  var u = new SetUrl();
-  u.DispURL(); 
+  var u = new getUrl();
+  u.dispURL(); 
 }
