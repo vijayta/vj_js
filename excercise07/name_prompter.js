dc23f78f7a42275ec7c73357f9a6b6617a9af52d
vijayta <vijayta.panchal@gmail.com>
2014-02-21 14:49:41 +0530

    excercise file name and OOPs fixes

--- excercise07/name_prompter.js
+++ excercise07/name_prompter.js
@@ -1,31 +1,31 @@
-function user(){
+function User(){
    var u = new Object();
    u.fname = this.setName('first');
    u.lname = this.setName('last');
    u.dispName = this.dispName;
    return u;
 }
-user.prototype.setName = function(name_part){
+User.prototype.setName = function(name_part){
   this.name_part = name_part;
   this.name_part = this.validate(prompt("Enter " + name_part + " Name:"));
   return this.name_part;
 }
-user.prototype.validate = function(name){
+User.prototype.validate = function(name){
   this.name = name;
-  //name == name.match()
-  if(name == "" || name == null || /^\s+$/.test(name)){
+  var name = name.trim();
+  if(name == "" || name == null){
     alert("you cant leave " + this.name_part + " name empty"); 
     return this.setName(this.name_part);
   }
   return this.name;
 }
 
-user.prototype.dispName = function(){
+User.prototype.dispName = function(){
   alert("Hello " + this.fname + " " + this.lname);
   var msg = document.getElementById('message');
   msg.innerHTML += "<p>Hello " + this.fname + " " + this.lname + ".</p>";  
 }
 window.onload = function(){ 
-  var user1 = new user();
+  var user1 = new User();
   user1.dispName(); 
 }
