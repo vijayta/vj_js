fb54e4f66a045df910de6a436d138b6884067eb1
vijayta <vijayta.panchal@gmail.com>
2014-03-28 11:52:51 +0530

    form validation

--- form_validation/reg_form.js
+++ form_validation/reg_form.js
@@ -1,7 +1,7 @@
-var user_input = document.getElementsByClassName('check');
-
 function UserInput(){
-  this.user_input = user_input;
+  this.user_input = document.getElementsByClassName('check');
+  this.user_desc = document.getElementById('user_desc').value;
+  this.notify = document.getElementById('notify_me');
 }
 UserInput.prototype.validateInfo = function(event) {
   for(var i = 0; i < this.user_input.length; i++) {
@@ -12,24 +12,27 @@ UserInput.prototype.validateInfo = function(event) {
     }  
   }
 }
-UserInput.prototype.desc = function(){
-  var user_desc01 = document.getElementById('user_desc').value;
-  if(user_desc01.length < 50){
+UserInput.prototype.validateDesc = function(){
+  if(this.user_desc.length < 50){
     alert("User Information('About me') Cant be less then 50 char.")
   }
 }
-UserInput.prototype.notify = function(){
-  var notify = document.getElementById('notify_me');
-  if(!notify.checked){
+UserInput.prototype.checkNotify = function(){
+  
+  if(!this.notify.checked){
     alert("Please check Receive Notification")
   }
 }
+UserInput.prototype.validateAndCheckNotification = function(event){
+  this.validateInfo(event);
+  this.validateDesc();
+  this.checkNotify();
+}
+
 window.onload = function(){  
   var on_submit = document.getElementById('user_info')
   on_submit.addEventListener('submit', function(event) {
     var u = new UserInput();
-    u.validateInfo(event);
-    u.desc();
-    u.notify();
+    u.validateAndCheckNotification(event);
   });
 }
