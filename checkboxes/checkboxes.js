eb04fc41fbad971a1f9a4b9ea04a1ac6639e9166
vijayta <vijayta.panchal@gmail.com>
2014-04-01 14:28:13 +0530

    checkbox choices excercise done

--- checkboxes/checkboxes.js
+++ checkboxes/checkboxes.js
@@ -4,14 +4,14 @@ function Choice() {
 
 Choice.prototype.userInput = function(ID) {
   this.id = ID;
-  for(var count = 1; count <= this.selectedOption.length; count++){
+  for(var count = 0; count <= this.selectedOption.length; count++){
     if (document.getElementById('item' + count).id != this.id) {
       document.getElementById('item' + count).checked = false;
-      this.selectedOption[count-1].parentNode.setAttribute('class', 'notActive')
+      this.selectedOption[count].parentNode.setAttribute('class', 'notActive')
     }
     else{
       document.getElementById('item' + count).checked = true;
-      this.selectedOption[count-1].parentNode.setAttribute('class', 'active')
+      this.selectedOption[count].parentNode.setAttribute('class', 'active')
     }
   }
 }
