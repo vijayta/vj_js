09960ac2d0d8bf483d00c141eecaadf01e5b1da7
vijayta <vijayta.panchal@gmail.com>
2014-04-03 12:31:04 +0530

    checklimit excercise minor fixes

--- excercise03/checkbox_limit.js
+++ excercise03/checkbox_limit.js
@@ -1,24 +1,24 @@
-function checkOperation() {
+function CheckboxList() {
   this.checkboxes = document.getElementsByName('list');
   this.none = document.getElementById('uncheckAll');
   this.selectionLimit = 3;
   this.init();
 }
 
-checkOperation.prototype.uncheckAll = function() {
+CheckboxList.prototype.uncheckAll = function() {
   for (var i = 0, len = this.checkboxes.length; i < len; i++) {
     if (this.none.checked) {
         this.checkboxes[i].checked = false;
     }
   }
 }
-checkOperation.prototype.uncheckNone = function(checkbox) {
+CheckboxList.prototype.uncheckNone = function(checkbox) {
   if (checkbox.checked && this.none.checked){
     this.none.checked = false;
   }  
 }
 
-checkOperation.prototype.createCheckboxList = function(checkbox) {
+CheckboxList.prototype.createCheckboxList = function(checkbox) {
   this.checkboxList = [];
   for (var i = 0, len = this.checkboxes.length; i < len; i++) {
     if (this.checkboxes[i].checked && this.checkboxes[i].id != checkbox.id) {
@@ -27,14 +27,14 @@ checkOperation.prototype.createCheckboxList = function(checkbox) {
   }
 }
 
-checkOperation.prototype.checkOverLimit = function(checkbox) {
+CheckboxList.prototype.checkOverLimit = function(checkbox) {
   if (this.checkboxList.length >= this.selectionLimit) {
     document.getElementById(checkbox.id).checked = false;
     alert("Only 3 days can be selected. You have already selected : " + this.checkboxList + " ");
   }
 }
 
-checkOperation.prototype.init = function() {
+CheckboxList.prototype.init = function() {
   var that = this;
   this.none.addEventListener('click', function() {
     that.uncheckAll();
@@ -50,5 +50,5 @@ checkOperation.prototype.init = function() {
 } 
 
 window.onload = function() {  
-  var check = new checkOperation();
+  var check = new CheckboxList();
 }
