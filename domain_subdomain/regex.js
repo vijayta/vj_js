6c63c80b5e38e9389889d5647419d1b363b62ba0
vijayta <vijayta.panchal@gmail.com>
2014-05-05 17:36:47 +0530

    extract domain and sub domain

--- domain_subdomain/regex.js
+++ domain_subdomain/regex.js
@@ -2,15 +2,10 @@ function UserInfo(){
   this.user_url = document.getElementById('user_input').value;
 }
 
-UserInfo.prototype.extractDomain = function(){
-  var urlParts = this.user_url.replace('http://','').replace('https://','').split(/[/?#]/);
-  this.domain = urlParts[0];
-  alert("Domain is: " + this.domain);
-}
-
-UserInfo.prototype.extractSubDomain = function(url){
-  var regex = /^([a-z]+\:\/{2})?([\w-]+\.[\w-]+\.\w+)$/;
-  regex.test(url);
+UserInfo.prototype.extractDomain = function(){  
+  var sub = this.user_url.replace(/^(www\.)?([^.]+)\.mydomain\.com/i,"$2");
+  var domain = this.user_url.match(/\(?(?:(http|https|ftp):\/\/)?(?:((?:[^\W\s]|\.|-|[:]{1})+)@{1})?((?:www.)?(?:[^\W\s]|\.|-)+[\.][^\W\s]{2,4}|localhost(?=\/)|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::(\d*))?([\/]?[^\s\?]*[\/]{1})*(?:\/?([^\s\n\?\[\]\{\}\#]*(?:(?=\.)){1}|[^\s\n\?\[\]\{\}\.\#]*)?([\.]{1}[^\s\?\#]*)?)?(?:\?{1}([^\s\n\#\[\]]*))?([\#][^\s\n]*)?\)?/gi);
+  console.log ("Sub Domain is : " + domain + " Sub Domain Name is : " + sub);
 }
 
 window.onload = function(){ 
@@ -18,8 +13,9 @@ window.onload = function(){
   var url = document.getElementById('user_input').value;
   submit.addEventListener('click', function() {
     var u = new UserInfo();
-//    u.checkDomain(url);
     u.extractDomain();
-    //u.extractSubDomain(url);
   });
 }
+
+
+
