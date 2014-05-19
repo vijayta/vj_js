7cc292de83c9bdb2f75102a043fc992a2813e296
vijayta <vijayta.panchal@gmail.com>
2014-05-19 11:34:16 +0530

    minor fix ;

--- domain_subdomain/regex.js
+++ domain_subdomain/regex.js
@@ -5,7 +5,7 @@ function UserInfo(){
 UserInfo.prototype.extractDomain = function(){  
   var sub = this.user_url.replace(/^(www\.)?([^.]+)\.mydomain\.com/i,"$2");
   var domain = this.user_url.match(/\(?(?:(http|https|ftp):\/\/)?(?:((?:[^\W\s]|\.|-|[:]{1})+)@{1})?((?:www.)?(?:[^\W\s]|\.|-)+[\.][^\W\s]{2,4}|localhost(?=\/)|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::(\d*))?([\/]?[^\s\?]*[\/]{1})*(?:\/?([^\s\n\?\[\]\{\}\#]*(?:(?=\.)){1}|[^\s\n\?\[\]\{\}\.\#]*)?([\.]{1}[^\s\?\#]*)?)?(?:\?{1}([^\s\n\#\[\]]*))?([\#][^\s\n]*)?\)?/gi);
-  console.log ("Sub Domain is : " + domain + " Sub Domain Name is : " + sub);
+  console.log ("Sub Domain is : " + sub + " Domain Name is : " + domain);
 }
 
 window.onload = function(){ 
