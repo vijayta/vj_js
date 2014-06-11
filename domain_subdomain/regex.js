9806429c5c2c1bf41e33d268a4aa5fdc416c2421
vijayta <vijayta.panchal@gmail.com>
2014-06-11 10:19:24 +0530

    domain sub domain exercise

--- domain_subdomain/regex.js
+++ domain_subdomain/regex.js
@@ -1,6 +1,7 @@
 function DomainSubdomain(){
   this.url = document.getElementById('user_input').value;
   this.regex = /^((https?|ftp):\/\/)?(www\.)?(([A-z0-9]+\.)*)([A-z0-9]+\.[A-z]{2,4})(\/(.)*)?$/;
+  this.init();
 }
 
 DomainSubdomain.prototype.validateUrl = function(){
@@ -15,6 +16,7 @@ DomainSubdomain.prototype.extractDomain = function(){
   this.domain = RegExp.$6;
   this.subDomain =  RegExp.$4.replace(/\.$/, '');
 }
+
 DomainSubdomain.prototype.showDomainSubdomain = function(){
   if(this.subDomain.length == 0) {
     alert("Domain is = " + this.domain);  
@@ -24,15 +26,19 @@ DomainSubdomain.prototype.showDomainSubdomain = function(){
   }
 }
 
-window.onload = function(){ 
+DomainSubdomain.prototype.init = function() {
   var submit = document.getElementById('submit');
   var url = document.getElementById('user_input').value;
   submit.addEventListener('click', function() {
-    var u = new DomainSubdomain();
-    u.validateUrl();
-    u.extractDomain();
-    u.showDomainSubdomain();
+    var url = new DomainSubdomain();
+    url.validateUrl();
+    url.extractDomain();
+    url.showDomainSubdomain();
   });
+}
+
+window.onload = function(){ 
+  var userInput = new DomainSubdomain();
 }
 
 
