534e6220264dd1e34e0cde3b2624524d8df83d9f
vijayta <vijayta.panchal@gmail.com>
2014-06-12 11:42:08 +0530

    domain subdomain

--- domain_subdomain/regex.js
+++ domain_subdomain/regex.js
@@ -1,18 +1,19 @@
+var Regex = {
+  URL: /^((https?|ftp):\/\/)?(www\.)?(([A-z0-9]+\.)*)([A-z0-9]+\.[A-z]{2,4})(\/(.)*)?$/
+}
+
 function DomainSubdomain(){
-  this.url = document.getElementById('user_input').value;
-  this.regex = /^((https?|ftp):\/\/)?(www\.)?(([A-z0-9]+\.)*)([A-z0-9]+\.[A-z]{2,4})(\/(.)*)?$/;
-  this.submit = document.getElementById('submit');
   this.init();
 }
 
 DomainSubdomain.prototype.validateUrl = function(event) {
-  this.result = this.url != null && this.url.trim && this.regex.test(this.url);  
-  if(!this.result) {
+  var result = this.url != null && this.url.trim && this.url.match(Regex.URL);  
+  if(!result) {
     alert('Please enter a valid url');
     event.preventDefault();
   }
   else{
-    return this.result;  
+    return (this.extractDomain(), this.showDomainSubdomain());
   }
 }
 
@@ -22,24 +23,26 @@ DomainSubdomain.prototype.extractDomain = function(){
 }
 
 DomainSubdomain.prototype.showDomainSubdomain = function(){
-  if(this.result){
-    if(this.subDomain.length == 0) {
-      alert("Domain is = " + this.domain);  
-    }
-    else{
-      alert("Domain is = " + this.domain + " Sub Domain is : " + this.subDomain); 
-    }
+  if(this.subDomain.length == 0) {
+    alert("Domain is = " + this.domain);  
+  }
+  else{
+    alert("Domain is = " + this.domain + " Sub Domain is : " + this.subDomain); 
   }
 }
 
 DomainSubdomain.prototype.init = function() {
+  this.url = document.getElementById('user_input').value;
+  this.submit = document.getElementById('submit');
+  this.bindEvent();
+}
+DomainSubdomain.prototype.bindEvent = function(){
+  var obj = this; 
   this.submit.addEventListener('click', function(event) {
-    var url = new DomainSubdomain();
-    url.validateUrl(event);
-    url.extractDomain();
-    url.showDomainSubdomain();
+    return obj.validateUrl(event);
   });
 }
+
 window.onload = function(){ 
-  var url = new DomainSubdomain();
+  var testUrl = new DomainSubdomain();
 }
